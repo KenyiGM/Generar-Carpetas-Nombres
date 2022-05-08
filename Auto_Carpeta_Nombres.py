477deb59a454d147d3beb61a883e12c00742e5c3
KenyiGM <91709827+KenyiGM@users.noreply.github.com>
2022-05-08 10:42:18 -0500

    Update Auto_Carpeta_Nombres.py

--- Auto_Carpeta_Nombres.py
+++ Auto_Carpeta_Nombres.py
@@ -3,16 +3,18 @@
 Autor : Kenyi Alexander Galindo Martinez
 
 """
-from os import mkdir
+def GenerarCarpeta():
+  from os import mkdir
+  with open('lista.txt') as file:
+    lista_de_nombres = file.readlines()
+    for nombre in lista_de_nombres:
+      try:
+        if nombre.find("\n") != -1:
+          nombre = nombre[:nombre.find("\n")]
+        mkdir(nombre)
+      except:
+        print(f"Ya esta creada la carpeta {nombre}")
+  file.close()
 
-with open('lista.txt') as file:
-  lista_de_nombres = file.readlines()
-  for nombre in lista_de_nombres:
-    try:
-      if nombre.find("\n") != -1:
-        nombre = nombre[:nombre.find("\n")]
-      mkdir(nombre)
-    except:
-      print(f"La carpeta {nombre} ya esta creada")
-
-file.close()
+if __name__== "__main__":
+  GenerarCarpeta()
